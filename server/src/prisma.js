3aef8d11bcf90beda28470cdd79eedbaae21ff4c
novellito <novellito7@gmail.com>
2018-12-19 22:05:13 -0500

    add before real work

--- server/src/prisma.js
+++ server/src/prisma.js
@@ -1,8 +1,6 @@
 import { Prisma } from 'prisma-binding';
 import { fragmentReplacements } from './resolvers/index';
 
-console.log(process.env.PRISMA_ENDPOINT);
-console.log(process.env.PRISMA_SECRET);
 const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
   endpoint: process.env.PRISMA_ENDPOINT,
